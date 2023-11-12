313d9a7d8d85f5fd6255a7924b175fe399a35a29
Kserhi <movie.to.48@gmail.com>
2023-11-12 18:02:06 +0200

    Initial commit

--- AddressBook-main/AddressBook-main/src/main/java/com/example/addressbook/HelloApplication.java
+++ AddressBook-main/AddressBook-main/src/main/java/com/example/addressbook/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.addressbook;
+﻿package com.example.addressbook;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("edit.fxml"));
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("edit.fxml"));
 
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Адресна книга");
