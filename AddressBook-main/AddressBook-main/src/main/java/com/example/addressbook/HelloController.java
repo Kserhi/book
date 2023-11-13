5d6ff168ceca81689573eeeebb5041cef5bdd589
Kserhi <movie.to.48@gmail.com>
2023-11-13 19:50:07 +0200

    модальне вікно

--- AddressBook-main/AddressBook-main/src/main/java/com/example/addressbook/HelloController.java
+++ AddressBook-main/AddressBook-main/src/main/java/com/example/addressbook/HelloController.java
@@ -1,14 +1,51 @@
 package com.example.addressbook;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
+    public HelloController() {
+    }
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    private Button btnAdd;
+    @FXML
+    void showDialog(ActionEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("second.fxml"));
+        try {
+            Stage stage = new Stage();
+            Scene scene = new Scene(fxmlLoader.load(), 500, 150);
+            stage.setScene(scene);
+
+            stage.setTitle("Редагування");
+            stage.setMinHeight(150);
+            stage.setMinWidth(500);
+            stage.setResizable(false);
+            stage.initModality(Modality.WINDOW_MODAL);
+            stage.initOwner(btnAdd.getScene().getWindow());
+            stage.show();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
