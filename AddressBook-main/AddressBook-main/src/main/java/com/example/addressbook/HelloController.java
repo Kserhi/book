06784bf84847ffaab732eb6d68993cb084778d77
Kserhi <movie.to.48@gmail.com>
2023-11-13 21:41:30 +0200

    window alerts

--- AddressBook-main/AddressBook-main/src/main/java/com/example/addressbook/HelloController.java
+++ AddressBook-main/AddressBook-main/src/main/java/com/example/addressbook/HelloController.java
@@ -4,27 +4,27 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label label;
 
     public HelloController() {
     }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 
     @FXML
     private Button btnAdd;
+
     @FXML
     void showDialog(ActionEvent event) {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloController.class.getResource("second.fxml"));
@@ -48,4 +48,39 @@ public class HelloController {
 
     }
 
+//    @FXML
+//    void new_Alert(ActionEvent event) {
+//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+//        alert.setTitle("Видалення");
+//        alert.setHeaderText("Results");
+//        alert.setContentText("Ви успішно видалили запис!");
+//        alert.showAndWait();
+//    }
+
+        @FXML
+    void new_Alert(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Видалення");
+        alert.setHeaderText("Results:");
+        alert.setContentText("Видалення може призвести до зміни порядку осіб у  Адресній книзі! ");
+        alert.showAndWait();
+    }
+//    @FXML
+//    void information_Alert(ActionEvent event) {
+//
+//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+//        alert.setTitle("Видалення");
+//        alert.setContentText("Ви впевненні, що хочете видалити запис? ");
+//
+//        Optional<ButtonType> result = alert.showAndWait();
+//
+//        if (result.isPresent() && result.get() == ButtonType.OK) {
+//            this.label.setText("Запис видалено!");
+//        } else {
+//            this.label.setText("Відмінено або немає вибору!");
+//        }
+//
+//
+//
+//    }
 }
