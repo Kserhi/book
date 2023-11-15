d78a0e886acb4bd387765f269bae110f08a3e454
Kserhi <movie.to.48@gmail.com>
2023-11-15 19:52:30 +0200

    4лаба

--- AddressBook-main/AddressBook-main/src/main/java/com/example/addressbook/HelloController.java
+++ AddressBook-main/AddressBook-main/src/main/java/com/example/addressbook/HelloController.java
@@ -8,22 +8,67 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.format.ResolverStyle;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 public class HelloController {
+
     @FXML
-    private Label label;
-
-    public HelloController() {
-    }
-
+    private Button btnDelete;
 
     @FXML
     private Button btnAdd;
+
+    @FXML
+    private Button btnOther;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private Button choose;
+
+    @FXML
+    private Label text;
+
+    Image image = new Image(getClass().getResourceAsStream("second.jpg"));
+
+    @FXML
+    void changeLabel(ActionEvent event) {
+
+        imageView.setImage(image);
+        text.setText("Ви успішно змінити картинку!");
+        choose.setDisable(true);
+    }
+
+    @FXML
+    public  void initialize(URL url, ResourceBundle resourceBundle){
+        btnOther.setOnAction(actionEvent ->{
+            FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("otherLabs.fxml"));
+            Stage stage=new Stage();
+            Scene scene=null;
+            try {
+                scene=new Scene(fxmlLoader.load(),600,600);
+
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+            stage.setTitle("Інші практичні");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+
+        });
+
+    }
 
     @FXML
     void showDialog(ActionEvent event) {
